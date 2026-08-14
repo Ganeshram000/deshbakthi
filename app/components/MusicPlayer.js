03cdd713aa7a191535eed8660b97b1e3fcedde48
Ganesh Ram <ganeshram8251@gmail.com>
2026-08-14 16:06:40 +0530

    Update website

--- app/components/MusicPlayer.js
+++ app/components/MusicPlayer.js
@@ -52,6 +52,7 @@ export default function MusicPlayer({ playlistId = PLAYLIST_ID }) {
       if (playerRef.current) { try { playerRef.current.destroy(); } catch (_) {} playerRef.current = null; }
       playerRef.current = new window.YT.Player(containerRef.current, {
         height: '200', width: '200',
+        videoId: 'dQw4w9WgXcQ',
         playerVars: { autoplay: 0, controls: 0, rel: 0, modestbranding: 1, enablejsapi: 1, playsinline: 1, origin },
         events: { onReady, onStateChange },
       });
